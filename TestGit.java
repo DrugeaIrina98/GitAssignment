b4530de822d1f17ef08d070882e35c8b46b6da98
DrugeaIrina98 <56608340+DrugeaIrina98@users.noreply.github.com>
2020-03-13 18:50:12 +0200

    Update TestGit.java

--- TestGit.java
+++ TestGit.java
@@ -2,7 +2,7 @@
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.print("Hello Git! The name of the license project is Facial recognition in baking system!");
+		System.out.print("Hello Git! The name of the license project is Facial recognition in banking system!");
 	}
 
 }
