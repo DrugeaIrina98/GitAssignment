d70cfa6209b93372d241286d228145272888c75f
DrugeaIrina98 <56608340+DrugeaIrina98@users.noreply.github.com>
2020-03-13 18:49:15 +0200

    Add files via upload

---
+++ TestGit.java
@@ -0,0 +1,8 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Git! The name of the license project is Facial recognition in baking system!");
+	}
+
+}
