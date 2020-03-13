00828ba5339786c2e09c8354a40f8b71dcecf824
DrugeaIrina98 <56608340+DrugeaIrina98@users.noreply.github.com>
2020-03-13 20:25:19 +0200

    Add files via upload
    Adding the Accountable interface in order to manage credits. On top of that, overriding the methods in the client class.

---
+++ Accountable.java
@@ -0,0 +1,4 @@
+public interface Accountable {
+public void ATMWithdraw(String iban,int desiredSum);
+public void infoCredit();
+}
--- Client.java
+++ Client.java
@@ -1,5 +1,5 @@
 
-public class Client {
+public abstract class Client implements Accountable {
 String name;
 String IBAN;
 double credit;
@@ -13,6 +13,18 @@ public Client(String name, String iBAN) {
 	this.name = name;
 	this.IBAN = iBAN;
 }
-
-
+@Override
+public void ATMWithdraw(String IBAN, int desiredsum) {
+	var MAX_WITHDRAW_SUM=50000;
+	if(desiredsum<MAX_WITHDRAW_SUM)
+	{
+		this.credit+=desiredsum;
+		System.out.print("The account with IBAN "+IBAN+" has withdrawed the asked sum.");
+	}
+}
+@Override
+public void infoCredit()
+{
+	System.out.print("The client "+this.name+" has the account with IBAN "+this.IBAN+" and has a credit of "+this.credit);
+}
 }
