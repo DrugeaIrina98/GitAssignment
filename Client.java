29df1bb0586c7c7ec6b3c5aa66587f425840b768
DrugeaIrina98 <56608340+DrugeaIrina98@users.noreply.github.com>
2020-03-13 19:00:34 +0200

    Add files via upload
    The class that manages the client, a main component of the project.

---
+++ Client.java
@@ -0,0 +1,18 @@
+
+public class Client {
+String name;
+String IBAN;
+double credit;
+
+public Client()
+	{
+	
+	}
+public Client(String name, String iBAN) {
+	super();
+	this.name = name;
+	this.IBAN = iBAN;
+}
+
+
+}
